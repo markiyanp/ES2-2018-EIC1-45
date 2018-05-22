c47f5ccc9bd2950de58d628a034fa7b91ee6c58a
pvmpa-iscteiulpt <pvmpa@iscte-iul.pt>
2018-05-22 18:42:10 +0100

    Adapted tests so that they actually work with the timelimit

--- ES2-2018-EIC1-45/src/jMetal/tests/TestjMetal.java
+++ ES2-2018-EIC1-45/src/jMetal/tests/TestjMetal.java
@@ -11,39 +11,42 @@ public class TestjMetal {
 //						   {"test4-should-not-appear", "Integer", "-10", "10", null, false}};
 //		OptimizationProcess.runOptimization(testD, "NSGAII", false);
 //
-//		Object[][] testD = {{"test1-should-not-appear-VAR-DOUBL", "Double", "-1.1", "2.2", null, false},
-//						   {"test2-should-appear-VAR-DOUBL", "Double", "-2.0", "2.0", null, true},
-//						   {"test3-should-appear-VAR-DOUBL", "Double", "-10.0", "10.0", null, true},
-//						   {"test4-should-not-appear-VAR-DOUBL", "Integer", "-10", "10", null, false}};
-//		Object[][] testOD = {
-//				{"test1-should-appear-OBJ-DOUBL", "Double", true},
-//				{"test2-should-not-appear-OBJ-DOUBL", "Integer", false},
-//				{"test3-should-appear-OBJ-DOUBL", "Double", true},
-//				{"test5-should-not-appear-OBJ-DOUBL", "Double", false}
-//		};
-//		OptimizationProcess.setObjectives(testOD);
-//		OptimizationProcess.setData(testD);
-//		OptimizationProcess.setAlgorithm("NSGAII");
-//		OptimizationProcess.setJar(false);
-//		OptimizationProcess.setProblemName("TestjMetal");
-//		OptimizationProcess.runOptimization();
-//		
-//		Object[][] testI = {{"test1-should-appear-VAR-INTEG", "Integer", "-1", "2", null, true},
-//				   {"test2-should-appear-VAR-INTEG", "Integer", "-2", "5", null, true},
-//				   {"test3-should-appear-VAR-INTEG", "Integer", "-10", "10", null, true},
-//				   {"test4-should-not-appear-VAR-INTEG", "Integer", "-10", "10", null, false}};
-//		Object[][] testOI = {
-//				{"test1-should-appear-OBJ-VAR-INTEG", "Integer", true},
-//				{"test2-should-not-appear-OBJ-VAR-INTEG", "Double", false},
-//				{"test3-should-appear-OBJ-VAR-INTEG", "Integer", true},
-//				{"test5-should-not-appear-OBJ-VAR-INTEG", "Integer", false}
-//		};
-//		OptimizationProcess.setObjectives(testOI);
-//		OptimizationProcess.setData(testI);
-//		OptimizationProcess.setAlgorithm("NSGAII");
-//		OptimizationProcess.setJar(false);
-//		OptimizationProcess.setProblemName("TestjMetal");
-//		OptimizationProcess.runOptimization();
+		Object[][] testD = {{"test1-should-not-appear-VAR-DOUBL", "Double", "-1.1", "2.2", null, false},
+						   {"test2-should-appear-VAR-DOUBL", "Double", "-2.0", "2.0", null, true},
+						   {"test3-should-appear-VAR-DOUBL", "Double", "-10.0", "10.0", null, true},
+						   {"test4-should-not-appear-VAR-DOUBL", "Integer", "-10", "10", null, false},
+						   {"test5-shoul-appear-VAR-DOUBL", "Double", "-10.0", "10.0", null, true}};
+		Object[][] testOD = {
+				{"test1-should-appear-OBJ-DOUBL", "Double", true},
+				{"test2-should-not-appear-OBJ-DOUBL", "Integer", false},
+				{"test3-should-appear-OBJ-DOUBL", "Double", true},
+				{"test5-should-not-appear-OBJ-DOUBL", "Double", false}
+		};
+		OptimizationProcess.setObjectives(testOD);
+		OptimizationProcess.setData(testD);
+		OptimizationProcess.setAlgorithm("NSGAII");
+		OptimizationProcess.setJar(false);
+		OptimizationProcess.setProblemName("TestjMetal");
+		OptimizationProcess.setTimelimit(100000);
+		OptimizationProcess.runOptimization();
+
+		Object[][] testI = { { "test1-should-appear-VAR-INTEG", "Integer", "-1", "2", null, true },
+				{ "test2-should-appear-VAR-INTEG", "Integer", "-1", "2", null, true },
+				{ "test3-should-appear-VAR-INTEG", "Integer", "-1", "2", null, true }
+				 };
+		Object[][] testOI = {
+		{"test1-should-appear-OBJ-INTEG", "Integer", true},
+		{"test2-should-not-appear-OBJ-INTEG", "Double", false},
+		{"test3-should-appear-OBJ-INTEG", "Integer", true},
+		{"test5-should-not-appear-OBJ-INTEG", "Integer", false}
+				};
+		OptimizationProcess.setObjectives(testOI);
+		OptimizationProcess.setData(testI);
+		OptimizationProcess.setAlgorithm("NSGAII");
+		OptimizationProcess.setJar(false);
+		OptimizationProcess.setTimelimit(100000);
+		OptimizationProcess.setProblemName("TestjMetal");
+		OptimizationProcess.runOptimization();
 		//SMSEMOA
 		
 
@@ -53,22 +56,23 @@ public class TestjMetal {
 //				   {"test4-should-not-appear", "Binary", "-10", "10", null, false}};
 //		OptimizationProcess.runOptimization(testB, "NSGAII", false);
 //
-//		Object[][] testB = {{"test1-should-appear-VAR-BINAR", "Binary", "-1", "2", null, true},
-//				   {"test2-should-appear-VAR-BINAR", "Binary", "-2", "5", null, true},
-//				   {"test3-should-appear-VAR-BINAR", "Binary", "-10", "10", null, true},
-//				   {"test4-should-not-appear-VAR-BINAR", "Binary", "-10", "10", null, false}};
-//		Object[][] testOB = {
-//				{"test1-should-appear-OBJ-VAR-BINAR", "Binary", true},
-//				{"test2-should-not-appear-OBJ-VAR-BINAR", "Integer", false},
-//				{"test3-should-appear-OBJ-VAR-BINAR", "Binary", true},
-//				{"test5-should-not-appear-OBJ-VAR-BINAR", "Binary", false}
-//		};
-//		OptimizationProcess.setObjectives(testOB);
-//		OptimizationProcess.setData(testB);
-//		OptimizationProcess.setAlgorithm("SMSEMOA");
-//		OptimizationProcess.setJar(false);
-//		OptimizationProcess.setProblemName("TestjMetal");
-//		OptimizationProcess.runOptimization();
+		Object[][] testB = {{"test1-should-appear-VAR-BINAR", "Binary", "-1", "2", null, true},
+				   {"test2-should-appear-VAR-BINAR", "Binary", "-2", "5", null, true},
+				   {"test3-should-appear-VAR-BINAR", "Binary", "-10", "10", null, true},
+				   {"test4-should-not-appear-VAR-BINAR", "Binary", "-10", "10", null, false}};
+		Object[][] testOB = {
+				{"test1-should-appear-OBJ-VAR-BINAR", "Binary", true},
+				{"test2-should-not-appear-OBJ-VAR-BINAR", "Integer", false},
+				{"test3-should-appear-OBJ-VAR-BINAR", "Binary", true},
+				{"test5-should-not-appear-OBJ-VAR-BINAR", "Binary", false}
+		};
+		OptimizationProcess.setObjectives(testOB);
+		OptimizationProcess.setData(testB);
+		OptimizationProcess.setAlgorithm("SMSEMOA");
+		OptimizationProcess.setJar(false);
+		OptimizationProcess.setTimelimit(100000);
+		OptimizationProcess.setProblemName("TestjMetal");
+		OptimizationProcess.runOptimization();
 //
 //		
 //		Object[][] test1 = {{"test1-singular-variable", "Double", "-1.1", "2.2", null, true }};
