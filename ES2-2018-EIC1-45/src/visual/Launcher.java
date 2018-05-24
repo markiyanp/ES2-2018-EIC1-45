dbae78aaa44f64c7732ccb07561c1808c92cd5a3
Markiyan <markiyan@iscte-iul.pt>
2018-05-24 15:29:17 +0100

    Modifications

--- ES2-2018-EIC1-45/src/visual/Launcher.java
+++ ES2-2018-EIC1-45/src/visual/Launcher.java
@@ -48,54 +48,6 @@ public class Launcher extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
-<<<<<<< HEAD
-=======
-//	public File readFromJARFile() throws IOException {
-//		InputStream inputStream = Launcher.class.getResourceAsStream("/config.xml");
-//		File return_file = new File("Resources/config.xml");
-//		try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-//			FileOutputStream outputStream = new FileOutputStream(return_file);
-//			int read = 0;
-//			byte[] bytes = new byte[1024];
-//			while ((read = inputStream.read(bytes)) != -1) {
-//				outputStream.write(bytes, 0, read);
-//			}
-//			outputStream.close();
-//		} catch (IOException e) {
-//		}
-//
-//		return return_file;
-//	}
-//	
-//	public File readFromJarFile(){
-//		File file = null;
-//	    String resource = "/Resources/config.xml";
-//	    URL res = getClass().getResource(resource);
-//	    if (res.toString().startsWith("jar:")) {
-//	        try {
-//	            InputStream input = getClass().getResourceAsStream(resource);
-//	            file = File.createTempFile("tempfile", ".tmp");
-//	            OutputStream out = new FileOutputStream(file);
-//	            int read;
-//	            byte[] bytes = new byte[1024];
-//
-//	            while ((read = input.read(bytes)) != -1) {
-//	                out.write(bytes, 0, read);
-//	            }
-//	            file.deleteOnExit();
-//	        } catch (IOException ex) {
-//	        }
-//	    } else {
-//	        //this will probably work in your IDE, but not from a JAR
-//	        file = new File(res.getFile());
-//	    }
-//
-//	    if (file != null && !file.exists()) {
-//	        throw new RuntimeException("Error: File " + file + " not found!");
-//	    }
-//	    return file;
-//	}
-
 	/**
 	 * The main
 	 * 
@@ -103,7 +55,6 @@ public class Launcher extends JFrame {
 	 * @throws IOException
 	 * @throws URISyntaxException
 	 */
->>>>>>> branch 'master' of https://github.com/markiyan-iscteiulpt/ES2-2018-EIC1-45.git
 	public static void main(String[] args) throws IOException, URISyntaxException {
 		new Launcher();
 	}
