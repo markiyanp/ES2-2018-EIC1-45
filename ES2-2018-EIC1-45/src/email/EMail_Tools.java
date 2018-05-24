ad66af912a7794d3f5c4379bc9081c050fdc0605
pvmpa-iscteiulpt <pvmpa@iscte-iul.pt>
2018-05-24 23:31:27 +0100

    Restored testJMetal, sendProgressEmail(int), fixed ProgC

--- ES2-2018-EIC1-45/src/email/EMail_Tools.java
+++ ES2-2018-EIC1-45/src/email/EMail_Tools.java
@@ -10,18 +10,24 @@ import org.apache.commons.mail.MultiPartEmail;
 import org.apache.commons.mail.SimpleEmail;
 
 /**
- * Harbors several functions that are useful for the e-mail functionality of this program.
+ * Harbors several functions that are useful for the e-mail functionality of
+ * this program.
+ * 
  * @author pvmpa-iscteiulpt
  *
  */
 public class EMail_Tools {
-	
-	
-	protected static Object[] mailProviderToSMTP(String email){
+
+	// this class should never be instatiated!
+	private EMail_Tools() {
+
+	}
+
+	protected static Object[] mailProviderToSMTP(String email) {
 		int i;
-		for (i = 0; i < email.length(); i++){
-			if (email.charAt(i) == '@'){
-				email = email.substring(i+1);
+		for (i = 0; i < email.length(); i++) {
+			if (email.charAt(i) == '@') {
+				email = email.substring(i + 1);
 			}
 		}
 		Object[] smtp_and_port = new Object[3];
@@ -37,17 +43,16 @@ public class EMail_Tools {
 			smtp_and_port[2] = Boolean.FALSE;
 			return smtp_and_port;
 		}
-		
+
 		throw new IllegalArgumentException("WARNING: Invalid e-mail detected! Failed to parse provider!");
 	}
-	
-	
+
 	public static boolean checkAuth(String userAddr, String userPw, String reason) {
-		
+
 		if (reason.isEmpty()) {
 			throw new IllegalArgumentException("You must provide a reason to check authentication.");
 		}
-		
+
 		try {
 			System.out.println("Attempting to send warning e-mail");
 			sendMail(userAddr, userPw, userAddr, null, "[OPTIMIZATION PROGRAM] User activity detected", reason, null);
@@ -56,10 +61,11 @@ public class EMail_Tools {
 			e.printStackTrace();
 			return false;
 		}
-		
+
 	}
-	
-	/**Sends an e-mail.
+
+	/**
+	 * Sends an e-mail.
 	 * 
 	 * @param userAddr
 	 * @param userPw
@@ -71,96 +77,100 @@ public class EMail_Tools {
 	 * @author pvmpa-iscteiulpt
 	 * @throws EmailException
 	 */
-	public static void sendMail(
-			String userAddr, 
-			String userPw, 
-			String destinationAddr,
-			String[] cc,
-			String subject, 
-			String body, 
-			String attachmentPath) throws EmailException{
-		
-		//Get the appropriate SMTP server. If this fails, the entire sequence is aborted.
+	public static void sendMail(String userAddr, String userPw, String destinationAddr, String[] cc, String subject,
+			String body, String attachmentPath) throws EmailException {
+
+		// Get the appropriate SMTP server. If this fails, the entire sequence is
+		// aborted.
 		Object[] smtp_and_port = mailProviderToSMTP(userAddr);
-		
+
 		boolean validAttachment = false;
 		boolean validCc = false;
-		
+
 		if (attachmentPath != null) {
 			File file = new File(attachmentPath);
-			if ((!file.isDirectory() && file.exists())){
+			if ((!file.isDirectory() && file.exists())) {
 				validAttachment = true;
 				System.out.println("Attachment detected!");
 			}
 		}
-		
-		if (cc != null){ 
-			//chained if required or else we might get a null pointer exception
+
+		if (cc != null) {
+			// chained if required or else we might get a null pointer exception
 			if (cc.length > 0) {
 				validCc = true;
 			}
 		}
-		
-		if (validAttachment){
-			
+
+		if (validAttachment) {
+
 			File file = new File(attachmentPath);
 			System.out.println(file.getName());
-			
+
 			EmailAttachment attachment = new EmailAttachment();
 			attachment.setPath(attachmentPath);
 			attachment.setDisposition(EmailAttachment.ATTACHMENT);
 			attachment.setDescription("ES2-2018-EIC1-45");
 			attachment.setName(file.getName());
-			
+
 			MultiPartEmail email = new MultiPartEmail();
 			email.setHostName((String) smtp_and_port[0]);
 			email.setSmtpPort(Integer.parseInt((String) smtp_and_port[1]));
 			email.setSslSmtpPort((String) smtp_and_port[1]);
-			
+
 			email.setAuthenticator(new DefaultAuthenticator(userAddr, userPw));
-			
-			email.setSSLOnConnect((Boolean)smtp_and_port[2]);
-			
+
+			email.setSSLOnConnect((Boolean) smtp_and_port[2]);
+
 			email.setFrom(userAddr);
 			email.addTo(destinationAddr);
 			email.setSubject(subject);
 			email.setMsg(body);
-			
+
 			email.attach(attachment);
-			
-			if (validCc){
-				for (int i = 0; i < cc.length; i++){
+
+			if (validCc) {
+				for (int i = 0; i < cc.length; i++) {
 					email.addCc(cc[i]);
 				}
 			}
-			
+
 			System.out.println("Sending e-mail (with attachment)");
 			email.send();
-			
-		}
-		else{
+
+		} else {
 			Email email = new SimpleEmail();
-			
+
 			email.setHostName((String) smtp_and_port[0]);
 			email.setSmtpPort(Integer.parseInt((String) smtp_and_port[1]));
 			email.setSslSmtpPort((String) smtp_and_port[1]);
-			
+
 			email.setAuthenticator(new DefaultAuthenticator(userAddr, userPw));
-			email.setSSLOnConnect((Boolean)smtp_and_port[2]);
-			
+			email.setSSLOnConnect((Boolean) smtp_and_port[2]);
+
 			email.setFrom(userAddr);
 			email.addTo(destinationAddr);
 			email.setSubject(subject);
 			email.setMsg(body);
-			
-			if (validCc){
-				for (int i = 0; i < cc.length; i++){
+
+			if (validCc) {
+				for (int i = 0; i < cc.length; i++) {
 					email.addCc(cc[i]);
 				}
 			}
-			
+
 			System.out.println("Sending e-mail (no attachment)");
 			email.send();
 		}
 	}
+
+	public static void sendProgressMail(int progress) throws EmailException {
+		if (progress != 100)
+			sendMail("group45.optimization.bot@gmail.com", "", "group45.dummy.user.1@gmail.com", null,
+					"Your Optimization is currently at " + progress, "", null);
+		else
+			sendMail("group45.optimization.bot@gmail.com", "", "group45.dummy.user.1@gmail.com", null,
+					"Your Optimization is completed!", "", null);
+	}
+
 }
--- ES2-2018-EIC1-45/src/jMetal/ProgressChecker.java
+++ ES2-2018-EIC1-45/src/jMetal/ProgressChecker.java
@@ -8,19 +8,19 @@ import email.EMail_Tools;
 import visual.LeftPanel;
 
 public class ProgressChecker {
-	
+
 	private static final String CONFIRM_TITLE_FINISH = "Optimization finished";
 	private static final String CONFIRM_FINISH = "Optimization complete! Check out the Graphics tab for your results.";
 	private boolean email25 = false;
 	private boolean email50 = false;
 	private boolean email75 = false;
-	
+
 	private boolean useJar;
-	
-	public ProgressChecker(boolean useJar){
+
+	public ProgressChecker(boolean useJar) {
 		this.useJar = useJar;
 	}
-	
+
 	/**
 	 * checks test progress, if progress is 25,50,75 or 100% an email is sent to
 	 * user
@@ -33,34 +33,50 @@ public class ProgressChecker {
 			numberTests = 500;
 		else
 			numberTests = 2500;
-		double progress =  ((double)testNumber) / numberTests;
-		try {
-			int act_prog = (int) (progress*100);
-			LeftPanel.setProgress(act_prog);
-			if (progress >= 0.25 && !email25) {
-				EMail_Tools.sendProgressMail(25);
-				email25 = true;
+		double progress = ((double) testNumber) / numberTests;
+		int act_prog = (int) (progress * 100);
+		LeftPanel.setProgress(act_prog);
+		if (progress >= 0.25 && !email25) {
+			new EmailSender(25);
+			email25 = true;
+		}
+		if (progress >= 0.5 && !email50) {
+			new EmailSender(50);
+			email50 = true;
+		}
+		if (progress >= 0.75 && !email75) {
+			new EmailSender(75);
+			email75 = true;
+		}
+		if (progress == 1) {
+			new Thread() {
+				public void run() {
+					JOptionPane.showMessageDialog(null, CONFIRM_FINISH, CONFIRM_TITLE_FINISH, 1);
+				}
+			}.start();
+			new EmailSender(100);
+		}
+
+	}
+
+	private class EmailSender extends Thread {
+		private final String WARNING_FAILED_MAIL = "Failed to send progress e-mail!";
+		private final String WARNING_TITLE_FAILED_MAIL = "E-Mail failure";
+		
+		public int progress = 0;
+
+		public EmailSender(int prog) {
+			this.progress = prog;
+			this.start();
+		}
+
+		public void run() {
+			try {
+				EMail_Tools.sendProgressMail(progress);
+			} catch (EmailException e) {
+				JOptionPane.showMessageDialog(null, WARNING_FAILED_MAIL, WARNING_TITLE_FAILED_MAIL, 0);
+				e.printStackTrace();
 			}
-			if (progress >= 0.5 && !email50) {
-				EMail_Tools.sendProgressMail(50);
-				email50 = true;
-			}
-			if (progress >= 0.75 && !email75) {
-				EMail_Tools.sendProgressMail(75);
-				email75 = true;
-			}
-			if (progress == 1) {
-				new Thread() {
-					public void run() {
-						JOptionPane.showMessageDialog(null,
-								CONFIRM_FINISH,
-								CONFIRM_TITLE_FINISH, 1);
-					}
-				}.start();
-				EMail_Tools.sendProgressMail(100);
-			}
-		} catch (EmailException e) {
-			e.printStackTrace();
 		}
 	}
 }
